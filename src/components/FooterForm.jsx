1a825d562f55edd9b39a3689fc1f1c3c7f79a739
RobertZap <zapalowicz.robert@gmail.com>
2023-08-17 15:19:52 +0200

    input handling, prepare to POST data

--- src/components/FooterForm.jsx
+++ src/components/FooterForm.jsx
@@ -1,43 +1,59 @@
 import { useState } from "react";
+import axios, { Axios } from "axios";
 
 import "/src/scss/FooterForm.scss";
 
 export default function FooterForm() {
-    const [inputName, setInputName] = useState("");
-    const [inputMail, setInputMail] = useState("");
-    const [inputText, setInputText] = useState("");
+    const url = "http://localhost:3000/";
+    const [data, setData] = useState({
+        name: "",
+        mail: "",
+        message: "",
+    });
 
-    const handleChangeName = (event) => {
-        setInputName(event.target.value);
-    };
-    const handleChangeMail = (event) => {
-        setInputMail(event.target.value);
-    };
-    const handleChangeText = (event) => {
-        setInputText(event.target.value);
-    };
+    function submit(e) {
+        e.preventDefault();
+        axios
+            .post(url, {
+                name: data.name,
+                mail: data.mail,
+                message: data.message,
+            })
+            .then((res) => {
+                console.log(res.data);
+            });
+    }
+
+    function handle(e) {
+        const newdata = { ...data };
+        newdata[e.target.id] = e.target.value;
+        setData(newdata);
+        console.log(newdata);
+    }
 
     return (
-        <form className="form">
+        <form className="form" onSubmit={(e) => submit(e)}>
             <div className="form_first_section">
                 <label className="form_text">
                     <p className="form_text_paragraph">Wpisz swoje imię</p>
                     <input
                         className="form_text_input"
+                        id="name"
                         type="text"
                         name="Imię"
-                        value={inputName}
-                        onChange={handleChangeName}
+                        value={data.name}
+                        onChange={(e) => handle(e)}
                     />
                 </label>
                 <label className="form_mail">
                     <p className="form_mail_paragraph">Wpisz swoj email</p>
                     <input
                         className="form_mail_input"
+                        id="mail"
                         type="email"
                         name="email"
-                        value={inputMail}
-                        onChange={handleChangeMail}
+                        value={data.mail}
+                        onChange={(e) => handle(e)}
                     />
                 </label>
             </div>
@@ -45,8 +61,9 @@ export default function FooterForm() {
                 <p className="form_area_paragraph">Wpisz swoja wiadomośc</p>
                 <textarea
                     className="form_area_input"
-                    value={inputText}
-                    onChange={handleChangeText}
+                    id="message"
+                    value={data.message}
+                    onChange={(e) => handle(e)}
                 ></textarea>
             </label>
             <button className="form_button" type="submit">
